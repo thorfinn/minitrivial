c281c1842ffd97fb4d811cc573bdf8944ab64cff
fran <fran@fran-desktop.(none)>
2013-03-11 18:25:52 +0100

    first commit

---
+++ src/javaapplication4/jLabel1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author fran
+ */
+class jLabel1 {
+
+    static Point getLocation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setLocation(Point posicion_del_label) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
